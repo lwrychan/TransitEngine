5ade506398126cb0178b301259c409f178058dc9
L4852 <122051988+lwrychan@users.noreply.github.com>
2026-08-20 17:31:22 -0700

    Update PhysicalGridView.hpp

--- include/render/PhysicalGridView.hpp
+++ include/render/PhysicalGridView.hpp
@@ -9,7 +9,6 @@ struct RenderingConfig;
 
 namespace render
 {
-    // Editor-only metre grid. It is a render aid, not part of the network model.
     void drawPhysicalGridView(
         const network::PhysicalNetwork& physicalNetwork,
         const RenderingConfig& config);
